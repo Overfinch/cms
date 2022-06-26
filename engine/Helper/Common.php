bf2a99f644cf592e288e5cfbd867b868fbf0ebb7
Overfinch <bundichmail@gmail.com>
2022-06-26 23:53:46 +0300

    Update Common.php

--- engine/Helper/Common.php
+++ engine/Helper/Common.php
@@ -3,18 +3,18 @@
 namespace Engine\Helper;
 
 class Common{
-    function isPost(){ // проверка является ли запрос методом POST
+    static function isPost(){ // проверка является ли запрос методом POST
         if($_SERVER['REQUEST_METHOD'] == 'POST'){
             return true;
         }
         return false;
     }
 
-    function getMethod(){ // возвращает метод запроса
+    static function getMethod(){ // возвращает метод запроса
         return $_SERVER['REQUEST_METHOD'];
     }
 
-    function getPathUrl(){ // возвращает URL запроса
+    static function getPathUrl(){ // возвращает URL запроса
         $pathUrl = $_SERVER['REQUEST_URI'];
 
         if($position = strpos($pathUrl, '?')){ // обрезаем GET данные если есть
